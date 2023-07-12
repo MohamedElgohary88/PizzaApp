8ff08a235e6192a6a75d060fa0ee286ce93719e8
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-12 22:23:04 +0300

    Initial commit

---
+++ app/src/main/java/com/chocolate/pizzaapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.chocolate.pizzaapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.chocolate.pizzaapp.ui.theme.PizzaAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PizzaAppTheme {
+
+            }
+        }
+    }
+}
